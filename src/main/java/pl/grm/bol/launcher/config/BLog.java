ca744e2a3dff056314350ba83cc6f46a4cfb96a4
Bolo <mastermast11@wp.pl>
2014-11-15 19:06:44 +0100

    .

--- src/main/java/pl/grm/bol/launcher/config/BLog.java
+++ src/main/java/pl/grm/bol/launcher/config/BLog.java
@@ -8,39 +8,38 @@ import javax.swing.JTextArea;
 import pl.grm.bol.lib.FileOperation;
 
 public class BLog {
-	private Logger		logger;
-	private JTextArea	console;
-	
+	private Logger logger;
+	private JTextArea console;
+
 	public BLog() {
 		Logger loggerR = null;
 		try {
-			loggerR = FileOperation.setupLauncherLogger(ConfigHandler.class);
-		}
-		catch (ClassNotFoundException | IllegalArgumentException | IllegalAccessException
-				| NoSuchFieldException e) {
+			loggerR = FileOperation.setupLogger(ConfigHandler.class);
+		} catch (ClassNotFoundException | IllegalArgumentException
+				| IllegalAccessException | NoSuchFieldException e) {
 			e.printStackTrace();
 		}
 		this.logger = loggerR;
 	}
-	
+
 	public synchronized void log(Level level, String msg, Throwable thrown) {
 		logger.log(level, msg, thrown);
 		if (console != null) {
 			console.append(msg + "\n");
 		}
 	}
-	
+
 	public synchronized void info(String msg) {
 		logger.info(msg);
 		if (console != null) {
 			console.append(msg + "\n");
 		}
 	}
-	
+
 	public JTextArea getConsole() {
 		return console;
 	}
-	
+
 	public void setConsole(JTextArea console) {
 		this.console = console;
 	}
